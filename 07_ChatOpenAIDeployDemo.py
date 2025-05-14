b398d4fffd6bea6c814982eaf50488bcdea93932
Srikanth Hosur <srikanth.manjunath@tietoevry.com>
2025-05-14 13:39:44 +0530

    fixed the st.text_input

--- 07_ChatOpenAIDeployDemo.py
+++ 07_ChatOpenAIDeployDemo.py
@@ -12,7 +12,7 @@ if not OPENAI_API_KEY:
 
 llm=ChatOpenAI(model="gpt-4o",api_key=OPENAI_API_KEY)
 
-question = input("Enter the question : ")
+question = st.text_input("Enter the question : ")
 
 if question:
     response = llm.invoke(question)
